4530d31165098df04d661416bed0c27f7ea44c65
Jerry Rolience <jerryrawlings892@gmail.com>
2025-04-02 11:32:43 +0300

    Refactor test-email route to include response in JSON and handle error in case of failure

--- app/api/test-email/route.ts
+++ app/api/test-email/route.ts
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    await sendEmail("welcome", "jerryrawlings892@gmail.com", {
+    const response = await sendEmail("welcome", "jerryrawlings892@gmail.com", {
       fullName: "Jerry Seinfeld",
     });
-    return NextResponse.json({ success: true });
+    return NextResponse.json({ success: true, response });
   } catch (error) {
     return NextResponse.json(
-      { error: "Failed to send test email" },
+      { error: error || "Failed to send test email" },
       { status: 500 }
     );
   }
